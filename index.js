3e0c9302d96009017bc49bf4d356bf7473045cb5
Priyanshu Soni <sonipriyanshusoni964@gmail.com>
2023-06-22 16:59:57 +0530

    back to normal

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const openMenu = () => {
-document.querySelector('aside').className='active';
-}
-const closeMenu = () =>{
-    document.querySelector('aside').className=''
-}
-document.getElementById('menu').onclick=e=>{
-e.preventDefault();
-openMenu();
-}
-document.getElementById('clos').onclick  = e =>{
-    closeMenu();
-}
+    document.querySelector('aside').className='active';
+    }
+    const closeMenu = () =>{
+        document.querySelector('aside').className=''
+    }
+    document.getElementById('menu').onclick=e=>{
+    e.preventDefault();
+    openMenu();
+    }
+    document.getElementById('clos').onclick  = e =>{
+        closeMenu();
+    }
